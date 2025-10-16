f8cead15ff5580d5169ccdfc8128f18bd011d515
Mouayad <muayad.perun@outlook.com>
2025-10-16 14:13:49 +0300

    feat(scaffold): add bodyBackgroundColor

--- lib/src/theme/themes/default_theme_no_secondary_border_variant.dart
+++ lib/src/theme/themes/default_theme_no_secondary_border_variant.dart
@@ -33,6 +33,7 @@ import 'package:shadcn_ui/src/theme/components/select.dart';
 import 'package:shadcn_ui/src/theme/components/separator.dart';
 import 'package:shadcn_ui/src/theme/components/sheet.dart';
 import 'package:shadcn_ui/src/theme/components/sidebar.dart';
+import 'package:shadcn_ui/src/theme/components/sidebar_scaffold.dart';
 import 'package:shadcn_ui/src/theme/components/slider.dart';
 import 'package:shadcn_ui/src/theme/components/sonner.dart';
 import 'package:shadcn_ui/src/theme/components/switch.dart';
@@ -1126,5 +1127,10 @@ class ShadDefaultThemeNoSecondaryBorderVariant extends ShadThemeVariant {
       mobileWidth: 288,
       mobileBreakPoint: 768,
     );
+  }
+
+  @override
+  ShadSidebarScaffoldTheme sidebarScaffoldTheme() {
+    return const ShadSidebarScaffoldTheme();
   }
 }
